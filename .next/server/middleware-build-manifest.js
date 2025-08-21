0b891eed7c26dae8d2a5f597b724e61357106051
Siddamnandas <128347410+Siddamnandas@users.noreply.github.com>
2025-08-21 05:40:55 +0530

    commit
    commit

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-ea021417016feeca.js","static/chunks/4bd1b696-2b95f9b795e01c06.js","static/chunks/684-b153eafc1c00ce00.js","static/chunks/main-app-f38f0d9153b95312.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-ea021417016feeca.js","static/chunks/framework-29ac49a6a3fd316f.js","static/chunks/main-c09f9dcdf4e52331.js","static/chunks/pages/_app-da15c11dea942c36.js"],"/_error":["static/chunks/webpack-ea021417016feeca.js","static/chunks/framework-29ac49a6a3fd316f.js","static/chunks/main-c09f9dcdf4e52331.js","static/chunks/pages/_error-cc3f077a18ea1793.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
